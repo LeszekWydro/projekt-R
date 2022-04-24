bb8a8c9214ce9a54c3390d9ddb81db1884da178e
Leszek Wydro <leszek.wydro@gmail.com>
2022-04-24 20:19:37 +0200

    add reducer

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,18 +1,21 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
 import Button from '../Button/Button.js';
+import { useDispatch } from 'react-redux';
 
 const ColumnForm = props => {
+
+    const dispatch = useDispatch();
 
     const [title, setTitle] = useState(' ');
     const [icon, setIcon] = useState(' ');
 
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title, icon: icon});
-        setIcon('');
+        dispatch({ type: 'ADD_COLUMN', newColumn: { title, icon } });
         setTitle('');
-    }; 
+        setIcon('');
+     };
 
    
 	return (
