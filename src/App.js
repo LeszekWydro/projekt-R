7bda2a3a2e0570975e7bbeae60a5b1986a01a01c
Leszek Wydro <leszek.wydro@gmail.com>
2022-04-27 19:55:54 +0200

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import Container from './components/Container/Container.js';
 import Hero from './components/Hero/Hero.js'
 import SearchForm from './components/SearchForm/SearchForm.js'
 import List from './components/List/List.js'
+import NavBar from './components/NavBar/NavBar.js';
 
 
 const App = () => {
   return (
-    <Container>
-      <Hero />
-      <SearchForm />
-      <List />
-    </Container>
+    <main>
+      <NavBar />
+      <Container>
+        <Hero />
+        <SearchForm />
+        <List />
+      </Container>
+    </main>
   );
 };
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import styles from './NavBar.module.scss';
+
+const NavBar = () => {
+  return (
+    <nav>
+        <div className={styles.container}>
+          <a href="/"><span className={ styles.icon + ' fa fa-tasks'} /></a>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/Favorite">Favorite</a></li>
+            <li><a href="/About">About</a></li>
+          </ul>
+        </div>
+    </nav>
+  );
+};
+
+export default NavBar;
